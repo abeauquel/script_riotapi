f10e491d3d0bc2a9635b3168cb8630bf7127f1db
abeauquel <alexandre.beauquel@celiersintelligents.com>
2021-10-05 14:20:01 -0400

    premier coommit

---
+++ src/main/java/com/ige487/script_rio_api/ScriptRioApiApplication.java
@@ -0,0 +1,13 @@
+package com.ige487.script_rio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptRioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScriptRioApiApplication.class, args);
+    }
+
+}
